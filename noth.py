37bcfd3cff6bbf181d1be3b9e1e7ca0695b2a2ea
younis ibrahim <younisibrahimali@gmail.com>
2022-12-15 11:15:45 +0300

    learning modules

--- noth.py
+++ noth.py
@@ -1,11 +1,7 @@
-class Players:
-    print("plays in pitch")
+#modules are used to better organize our code,we break up our code into multiple files each file is called module should contain all functions classes which are related ,then we can import a module into another module .
+#or you can write import converter,but then you have to specify your function
 
+import converter
+from converter import lbs_to_kg
+print(lbs_to_kg(70))
 
-class Attackers(Players):
-#don't forget to write pass
-    pass
-
-
-attack =Attackers()
-print(attack)
