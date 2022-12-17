88792b8244b27c16384df3c2a706da55b381354c
younis ibrahim <younisibrahimali@gmail.com>
2022-12-17 13:46:39 +0300

    logical operators

--- noth.py
+++ noth.py
@@ -1,11 +1,20 @@
-hot_day =False
-cold_day = False
-if not hot_day:
-    print("it's a hot day drink plenty of water")
-elif cold_day:
-    print("wear many clothes")
+has_high_income = True
+has_good_credit = True
+if has_good_credit and  has_high_income:
+    print("allowed ")
 
-else:
-    print("it's a nice day")
 
-print("have a good day")
+if has_high_income and not has_good_credit:
+    print("allowed")
+
+
+if has_high_income or has_good_credit:
+    print("allowed")
+
+if has_high_income or not has_good_credit:
+    print("allowed")
+
+if not has_high_income and has_good_credit:
+    print("allowed")
+#and:both must be true or false
+#or:at least one must be true or false
