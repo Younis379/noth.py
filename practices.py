438dc5cbdf69d5ecfa043987919f951b4ca3a930
younis ibrahim <younisibrahimali@gmail.com>
2022-12-18 09:35:00 +0300

    for loop

--- practices.py
+++ practices.py
@@ -1,18 +1,7 @@
 
-while True:
-    answer = input(">").lower()
-    if answer =="start":
-        print("car ready to go")
-        break
-    elif answer =="stop":
-        print("car is stopping")
-        break
-    elif answer == "help":
-        print("""
-start-to start
- stop-to stop
- quit-to quit""")
-    elif answer=="quit":
-        break
-    else:
-        print("i don't understand that")
+list = [50,100,30]
+tot = 0
+for item in list:
+    tot+=item
+print(tot)
+
